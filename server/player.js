4b2e5a1f38608439584f708e42aca5b9a3cd391f
Michal R <misiak_pl@o2.pl>
2020-04-26 15:57:47 +0200

    Circular arena

--- server/player.js
+++ server/player.js
@@ -14,8 +14,16 @@ class Player {
         var t = d.getTime();
         var newPositionX = this.position.x + Math.cos(this.angle) * 10 * (t - this.lastUpdateTime) / settings.stepTime;
         var newPositionY = this.position.y + Math.sin(this.angle) * 10 * (t - this.lastUpdateTime) / settings.stepTime;
-        this.position.x = Math.abs(newPositionX) <= settings.worldSize ? newPositionX : this.position.x;
-        this.position.y = Math.abs(newPositionY) <= settings.worldSize ? newPositionY : this.position.y;
+        // circular arena
+        var r = Math.sqrt(newPositionX * newPositionX + newPositionY * newPositionY);
+        if (r <= settings.worldSize) {
+            this.position = { x: newPositionX, y: newPositionY };
+        } else {
+            this.position = { x: newPositionX * settings.worldSize / r, y: newPositionY * settings.worldSize / r };
+        }
+        // square arena
+        /*this.position.x = Math.abs(newPositionX) <= settings.worldSize ? newPositionX : this.position.x;
+        this.position.y = Math.abs(newPositionY) <= settings.worldSize ? newPositionY : this.position.y;*/
         this.lastUpdateTime = t;
     }
 }
